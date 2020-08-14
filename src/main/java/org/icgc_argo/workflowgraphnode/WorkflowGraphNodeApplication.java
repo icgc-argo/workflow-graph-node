909f9a99d26ab054f75db06628395b797b46b173
Dušan Andrić <Dusan.Andric@gmail.com>
2020-08-14 11:06:51 -0400

    initial spring commit

---
+++ src/main/java/org/icgc_argo/workflowgraphnode/WorkflowGraphNodeApplication.java
@@ -0,0 +1,13 @@
+package org.icgc_argo.workflowgraphnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowGraphNodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorkflowGraphNodeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/icgc_argo/workflowgraphnode/WorkflowGraphNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.icgc_argo.workflowgraphnode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowGraphNodeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
