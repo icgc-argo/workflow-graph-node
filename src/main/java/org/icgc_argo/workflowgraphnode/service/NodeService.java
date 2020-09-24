f9284eac167ef2745585279bbf28dfe2782762e3
Alex <lepsalex@gmail.com>
2020-09-24 19:30:01 -0400

    node service refactor to handle just pipeline management

--- src/main/java/org/icgc_argo/workflowgraphnode/service/NodeService.java
+++ src/main/java/org/icgc_argo/workflowgraphnode/service/NodeService.java
@@ -1,26 +1,15 @@
 package org.icgc_argo.workflowgraphnode.service;
 
-import com.pivotal.rabbitmq.RabbitEndpointService;
-import com.pivotal.rabbitmq.ReactiveRabbit;
-import com.pivotal.rabbitmq.source.Source;
-import com.pivotal.rabbitmq.stream.Transaction;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.icgc_argo.workflowgraphnode.components.Node;
-import org.icgc_argo.workflowgraphnode.config.AppConfig;
 import org.icgc_argo.workflowgraphnode.model.PipeStatus;
-import org.icgc_argo.workflowgraphnode.model.RunRequest;
-import org.icgc_argo.workflowgraphnode.rabbitmq.TopologyConfiguration;
-import org.icgc_argo.workflowgraphnode.workflow.RdpcClient;
+import org.icgc_argo.workflowgraphnode.rabbitmq.NodeConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.Disposable;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
-import java.time.Duration;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,45 +17,22 @@ import java.util.Map.Entry;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
-import static org.icgc_argo.workflow_graph_lib.utils.JacksonUtils.toMap;
-
 @Slf4j
 @Configuration
 public class NodeService {
 
-  private static final String INGEST = "httpIngest";
-  private static final String QUEUED_TO_RUNNING = "queuedToRunning";
-  private static final String RUNNING_TO_COMPLETE = "runningToComplete";
+  private static final String NODE = "nodeStream";
 
   private final Map<String, Disposable> pipelines = Collections.synchronizedMap(new HashMap<>());
 
   /** Dependencies */
-  private final RabbitEndpointService rabbit;
-
-  private final RdpcClient rdpcClient;
-  private final TopologyConfiguration topologyConfig;
-  private final Source<String> runRequestSource;
-  private final AppConfig appConfig;
-  private final Node node;
+  private final NodeConfiguration nodeConfiguration;
 
   @Autowired
-  public NodeService(
-      @NonNull RabbitEndpointService rabbit,
-      @NonNull RdpcClient rdpcClient,
-      @NonNull TopologyConfiguration topologyConfig,
-      @NonNull Source<String> runRequestSource,
-      @NonNull AppConfig appConfig,
-      @NonNull Node node) {
-    this.rabbit = rabbit;
-    this.rdpcClient = rdpcClient;
-    this.topologyConfig = topologyConfig;
-    this.runRequestSource = runRequestSource;
-    this.appConfig = appConfig;
-    this.node = node;
+  public NodeService(@NonNull NodeConfiguration nodeConfiguration) {
+    this.nodeConfiguration = nodeConfiguration;
 
-    startIngest();
-    startQueued();
-    startRunning();
+    startNode();
   }
 
   public Map<String, PipeStatus> getStatus() {
@@ -75,28 +41,12 @@ public class NodeService {
         .collect(Collectors.toUnmodifiableMap(Entry::getKey, Entry::getValue));
   }
 
-  public void stopIngest() {
-    pipelines.get(INGEST).dispose();
+  public void stopNode() {
+    pipelines.get(NODE).dispose();
   }
 
-  public void stopQueued() {
-    pipelines.get(QUEUED_TO_RUNNING).dispose();
-  }
-
-  public void stopRunning() {
-    pipelines.get(RUNNING_TO_COMPLETE).dispose();
-  }
-
-  public void startIngest() {
-    startPipe(INGEST, () -> ingestHttpJobs(this.runRequestSource));
-  }
-
-  public void startQueued() {
-    startPipe(QUEUED_TO_RUNNING, this::queuedToRunning);
-  }
-
-  public void startRunning() {
-    startPipe(RUNNING_TO_COMPLETE, this::runningToCompleteOrError);
+  public void startNode() {
+    startPipe(NODE, nodeConfiguration::nodeStream);
   }
 
   /**
@@ -131,117 +81,5 @@ public class NodeService {
     } else {
       return PipeStatus.ENABLED;
     }
-  }
-
-  /**
-   * The job of this pipe is to take the incoming messages from the Node's HTTP ingest endpoint and
-   * to place them on the run queue.
-   *
-   * @param runRequestSource The on demand source to send messages to.
-   * @return Returns the pipe in the form of a Disposable from the subscribed flux.
-   */
-  private Disposable ingestHttpJobs(Source<String> runRequestSource) {
-    return rabbit
-        .declareTopology(topologyConfig.inputTopology())
-        .createTransactionalProducerStream(String.class)
-        .route()
-        .toExchange(appConfig.getNodeProperties().getInput().getExchange())
-        .then()
-        .send(runRequestSource.source().doOnNext(i -> log.info("Trying to send: {}", i.get())))
-        .doOnError(
-            throwable ->
-                log.info(
-                    throwable.getLocalizedMessage())) // TODO: reject messages on error as well?
-        .transform(ReactiveRabbit.commitElseTerminate())
-        .doOnError(throwable -> log.info(throwable.getLocalizedMessage()))
-        .subscribe();
-  }
-
-  /**
-   * This pipe takes the queued messages from the run queue, attempts to run them as new workflow
-   * runs, and then places the runId of the running workflow onto the running message queue.
-   *
-   * @return Returns the pipe in the form of a Disposable from the subscribed flux.
-   */
-  private Disposable queuedToRunning() {
-    final Flux<Transaction<RunRequest>> incomingStream =
-        rabbit
-            .declareTopology(topologyConfig.inputTopology())
-            .createTransactionalConsumerStream(
-                appConfig.getNodeProperties().getInput().getQueue(), String.class)
-            .receive()
-            .doOnNext(tx -> log.info("WorkflowParamsFunction result: {}", tx.get()))
-            .<Transaction<RunRequest>>handle(
-                (tx, sink) -> {
-                  try {
-                    sink.next(tx.map(node.inputToRunning().apply(toMap(tx.get()))));
-                  } catch (Throwable e) {
-                    log.error(e.getLocalizedMessage());
-                    tx.reject();
-                  }
-                })
-            .doOnNext(tx -> log.info("Run request created: {}", tx.get()));
-
-    // TODO: Handle errors from the workflow API
-    final Flux<Transaction<String>> launchedWorkflowStream =
-        incomingStream
-            .doOnNext(item -> log.info("Attempting to run workflow with: {}", item.get()))
-            .flatMap(
-                tx ->
-                    rdpcClient
-                        .startRun(tx.get())
-                        .flatMap(response -> Mono.fromCallable(() -> tx.map(response))));
-
-    return rabbit
-        .declareTopology(topologyConfig.runningTopology())
-        .createTransactionalProducerStream(String.class)
-        .route()
-        .toExchange(appConfig.getNodeProperties().getRunning().getExchange())
-        .then()
-        .send(launchedWorkflowStream)
-        .subscribe(Transaction::commit);
-  }
-
-  /**
-   * This pipe checks the running jobs on the running message queue by querying the rdpc gateway to
-   * see if they have completed or errored out. On complete they are sent to the complete exchange
-   * and the transaction is commited, otherwise the transaction is rejected.
-   *
-   * @return
-   */
-  private Disposable runningToCompleteOrError() {
-    final Flux<Transaction<String>> completedWorkflows =
-        rabbit
-            .declareTopology(topologyConfig.runningTopology())
-            .createTransactionalConsumerStream(
-                appConfig.getNodeProperties().getRunning().getQueue(), String.class)
-            .receive()
-            .delayElements(Duration.ofSeconds(10))
-            .doOnNext(r -> log.info("Checking status of: {}", r.get()))
-            .filterWhen(
-                tx ->
-                    rdpcClient
-                        .getWorkflowStatus(tx.get())
-                        .map(
-                            s -> {
-                              log.info("Status for {}: {}", tx.get(), s);
-                              return "COMPLETE".equals(s);
-                            }))
-            .doOnDiscard(Transaction.class, tx -> tx.rollback(true));
-
-    // TODO: Handle executor error events, will probably require a dlq
-
-    return rabbit
-        .declareTopology(topologyConfig.completeTopology())
-        .createTransactionalProducerStream(String.class)
-        .route()
-        .toExchange(appConfig.getNodeProperties().getComplete().getExchange())
-        .and()
-        .whenNackByBroker()
-        .alwaysRetry(Duration.ofSeconds(5))
-        .then()
-        .send(completedWorkflows)
-        .doOnNext(tx -> log.info("Completed: {}", tx.get()))
-        .subscribe(Transaction::commit);
   }
 }
